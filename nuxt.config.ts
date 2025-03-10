4ed5cca9b4e0ead857bfe72ad23098c2fea64a97
Daniel <danzaric10@gmail.com>
2025-03-10 20:51:06 +0100

    Removed ss3 configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,17 +9,5 @@ export default defineNuxtConfig({
       secretKey:
         "fWKzzam9GdW3gtwGTQ7BBUmpCHgR7Nq9vQztdzVFaYy7cMwEKZlxq9204LMIYebi",
     },
-    uploads: {
-      drive: {
-        type: "s3",
-        baseUrl:
-          "https://43618a8104c6c47513ac3ef37665f8ac.eu.r2.cloudflarestorage.com/recipes-local",
-        bucket: "recipes-local",
-        endpoint: "recipes-local",
-        key: "hqHMYFwQvj3rKPj_Q7Vdkfh2ssxTA9VGJSmuhN0_",
-        region: "eu",
-        secret: "secret-key",
-      },
-    },
   },
 });
