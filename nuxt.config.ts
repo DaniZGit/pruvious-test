19e0d6b162dfe85e0a2a5f8cbaeb379ece3f9e2b
Daniel <danzaric10@gmail.com>
2025-03-10 20:05:41 +0100

    Added s3 storage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["pruvious"],
 
   pruvious: {
     jwt: {
-      secretKey: "fWKzzam9GdW3gtwGTQ7BBUmpCHgR7Nq9vQztdzVFaYy7cMwEKZlxq9204LMIYebi"
-    }
-  }
-})
+      secretKey:
+        "fWKzzam9GdW3gtwGTQ7BBUmpCHgR7Nq9vQztdzVFaYy7cMwEKZlxq9204LMIYebi",
+    },
+    uploads: {
+      drive: {
+        type: "s3",
+        baseUrl:
+          "https://43618a8104c6c47513ac3ef37665f8ac.eu.r2.cloudflarestorage.com/recipes-local",
+        bucket: "recipes-local",
+        endpoint: "recipes-local",
+        key: "hqHMYFwQvj3rKPj_Q7Vdkfh2ssxTA9VGJSmuhN0_",
+        region: "eu",
+        secret: "secret-key",
+      },
+    },
+  },
+});
