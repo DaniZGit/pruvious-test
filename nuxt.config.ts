7b6ce3761b332cdd1897ebaf43c310cdfd781623
Daniel <danzaric10@gmail.com>
2025-03-11 15:41:15 +0100

    Disabled page cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,18 @@ export default defineNuxtConfig({
       secretKey:
         "fWKzzam9GdW3gtwGTQ7BBUmpCHgR7Nq9vQztdzVFaYy7cMwEKZlxq9204LMIYebi",
     },
+    // uploads: {
+    //   drive: {
+    //     type: "s3",
+    //     baseUrl: process.env.SS3_BASE_URL ?? "",
+    //     bucket: process.env.SS3_BUCKET_NAME ?? "",
+    //     endpoint: process.env.SS3_ENDPOINT ?? "",
+    //     key: process.env.SS3_ACCESS_KEY ?? "",
+    //     region: process.env.SS3_REGION ?? "",
+    //     secret: process.env.SS3_SECRET_KEY ?? "",
+    //     forcePathStyle: true,
+    //   },
+    // },
+    pageCache: false,
   },
 });
